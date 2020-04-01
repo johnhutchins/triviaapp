3bcb10ceb11d2c523fa4b19a0d3739cfeb7b0194
John Hutchins <johnwallshutchins@gmail.com>
2020-04-01 10:29:17 -0400

    add files

--- src/Trivia.js
+++ src/Trivia.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
+import './Trivia.css'
 
-function Trivia() {
-    return (
-        <div>
-            <h1>The following city is the capital of what country?</h1>
-            <h2>capital</h2>
-        </div>
-    );
+export default class Trivia extends Component {
+    state = {
+        hasErrors: false,
+        data: {}
+    }
+
+    //this should be on click. for now componentDidMount hook is fine
+    componentDidMount() {
+        fetch('https://jwhrandomcountry.herokuapp.com/country')
+            .then(res => res.json())
+            .then(res => this.setState({ data: res }))
+    }
+
+    render() {
+        return (
+            <div className='triviaContainer'>
+                <h1>The following city is the capital of what country?</h1>
+                <h2>capital</h2>
+                <div>{this.data}</div>
+            </div>
+        )
+    }
 }
-
-export default Trivia;
