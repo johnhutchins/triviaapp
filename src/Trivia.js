806cd898faa2ae093d61dc941229e114e8b6b8ce
John Hutchins <johnwallshutchins@gmail.com>
2020-04-01 11:40:25 -0400

    to debug

--- src/Trivia.js
+++ src/Trivia.js
@@ -2,9 +2,13 @@ import React, { Component } from 'react';
 import './Trivia.css'
 
 export default class Trivia extends Component {
-    state = {
-        hasErrors: false,
-        data: {}
+    constructor(props) {
+        super(props)
+        this.state = {
+            name: 'test',
+            hasErrors: false,
+            data: {}
+        }
     }
 
     //this should be on click. for now componentDidMount hook is fine
@@ -18,15 +22,14 @@ export default class Trivia extends Component {
         })
             .then(res => res.json())
             .then(res => this.setState({ data: res }))
-        console.log("data = " + this.data)
     }
 
     render() {
         return (
             <div className='triviaContainer'>
                 <h1>The following city is the capital of what country?</h1>
-                <h2>capital</h2>
-                <div>{this.data}</div>
+                <h2>{this.data}</h2>
+                <h3>{this.name}</h3>
             </div>
         )
     }
