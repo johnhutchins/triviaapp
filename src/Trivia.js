0c8740f46c8f4a5f085cdecd219bba745ac11abd
John Hutchins <johnwallshutchins@gmail.com>
2020-04-01 09:22:51 -0400

    init files

---
+++ src/Trivia.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+
+function Trivia() {
+    return (
+        <div>
+            <h1>The following city is the capital of what country?</h1>
+            <h2>capital</h2>
+        </div>
+    );
+}
+
+export default Trivia;
