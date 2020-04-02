27f153f9562441570db9026714d1da2138ed74b5
John Hutchins <johnwallshutchins@gmail.com>
2020-04-01 20:42:45 -0400

    update

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import Trivia from './Trivia';
+import App from "./App"
 
 ReactDOM.render(
   <React.StrictMode>
-    <Trivia />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
