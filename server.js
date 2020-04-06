765c453f6540c8bb1ce68527c30bf118b778f33c
John Hutchins <johnwallshutchins@gmail.com>
2020-04-06 10:41:04 -0400

    push

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-const express = require('express');
-
-const app = express();
-const port = process.env.PORT || 5000;
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = process.env.PORT || 5000
 
 app.use(express.static('build'))
 
-app.get('/', (req, res) => {
-    //res.send({ express: 'Hello From Express' });
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
 });
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(port, () => console.log(`Listening on port ${port}`))
