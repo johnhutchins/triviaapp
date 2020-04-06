c6cded10b88953c0497a0cb041a587804689bb3f
John Hutchins <johnwallshutchins@gmail.com>
2020-04-06 10:25:05 -0400

    f

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.static('build'))
+
 app.get('/', (req, res) => {
     //res.send({ express: 'Hello From Express' });
 });
