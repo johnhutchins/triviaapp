b839f36539aa896e48c31411390fb7d6f697a402
John Hutchins <johnwallshutchins@gmail.com>
2020-04-06 12:42:49 -0400

    push

--- server.js
+++ server.js
@@ -3,10 +3,9 @@ const path = require('path')
 const app = express()
 const port = process.env.PORT || 5000
 
-app.use(express.static('build'))
+app.use(express.static(path.join(__dirname, 'build')));
 
-
-app.get('/', function (req, res) {
+app.get('/game', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
 });
 
